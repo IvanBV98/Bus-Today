839d2cc11d7e23d1f8e670e4427c64acbdc811c2
I. V. Bondarenko <bondarenko.ivan98@gmail.com>
2020-07-10 15:16:03 +0500

    secoondomer01

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,5 @@
-class Slider{
-    constructor({diaraction,moveTime,active,slider,auto}){
+class Slider {
+    constructor({ diaraction, moveTime, active, slider, auto }) {
         this.slider = document.querySelector(slider)
         this.sliderLine = this.slider.querySelector('.slider__line')
         this.next = this.slider.querySelector('.slider__next')
@@ -19,43 +19,43 @@ class Slider{
             height:${this.height}px;
             overflow:hidden;
         `
-        this.slides.forEach(el=>{
+        this.slides.forEach(el => {
             el.style = `
                 position:absolute;
                 width:${this.width}px;
                 height:${this.height}px;
             `
-            if(el != this.slides[this.active]){
+            if (el != this.slides[this.active]) {
                 el.style.transform = `translate${this.dir}(${this.moveSize}px)`
             }
-            if(el == this.slides[this.slides.length - 1]){
-                el.style.transform = `translate${this.dir}(${this.moveSize*-1}px)`
+            if (el == this.slides[this.slides.length - 1]) {
+                el.style.transform = `translate${this.dir}(${this.moveSize * -1}px)`
             }
         })
 
-        this.next.addEventListener('click',()=> this.move(this.next))
-        this.prev.addEventListener('click',()=> this.move(this.prev))
+        this.next.addEventListener('click', () => this.move(this.next))
+        this.prev.addEventListener('click', () => this.move(this.prev))
     }
-    move(btn){
+    move(btn) {
         let btnLeftOrRight = btn == this.next ? this.moveSize * -1 : this.moveSize
 
-        this.slides.forEach(el=>{
-          el.style.transition = '0ms'
-          if(el !== this.slides[this.active]){
-            el.style.transform = `translate${this.dir}(${btnLeftOrRight* -1}px)`
-          }  
+        this.slides.forEach(el => {
+            el.style.transition = '0ms'
+            if (el !== this.slides[this.active]) {
+                el.style.transform = `translate${this.dir}(${btnLeftOrRight * -1}px)`
+            }
         })
 
         this.slides[this.active].style.transition = `${this.timeMove}ms`
         this.slides[this.active].style.transform = `translate${this.dir}(${btnLeftOrRight}px)`
-        if(btn == this.next){
+        if (btn == this.next) {
             this.active++
-            if(this.active >= this.slides.length){
+            if (this.active >= this.slides.length) {
                 this.active = 0
             }
-        } else if(btn == this.prev){
+        } else if (btn == this.prev) {
             this.active--
-            if(this.active < 0){
+            if (this.active < 0) {
                 this.active = this.slides.length - 1
             }
         }
@@ -66,21 +66,37 @@ class Slider{
 
 
 
-const expmleSlider = new Slider({
-    diaraction: 'y',
+const headerSlider = new Slider({
+    diaraction: '',
     moveTime: '500',
     active: 0,
-    slider:'.myfirstslider',
+    slider: '.myfirstslider',
     auto: false,
     // opacity:true
-})
-const expmleSlider2 = new Slider({
-    diaraction: 'x',
-    moveTime: '500',
-    active: 0,
-    slider:'.mysecondslider',
-    auto: false,
-    // opacity:true
-})
+}),
+    newsSlider = new Slider({
+        diaraction: '',
+        moveTime: '500',
+        active: 0,
+        slider: '.mysecondslider',
+        auto: false,
+        // opacity:true
+    })
 
 console.log(expmleSlider)
+
+// main
+
+// secoondomer
+class Time {
+    constructor({ month, daynum, day, hour, minutes, second }) {
+        this.month = document.querySelector(month)
+        this.daynum = document.querySelector(daynum)
+        this.day = document.querySelector(day)
+        this.hour = document.querySelector(hour)
+        this.minutes = document.querySelector(minutes)
+        this.second = document.querySelector(second)
+        if (this.month.toUpperCase == 'JANUARY' || this.month.toUpperCase == 'MARCH' || this.month.toUpperCase == 'MAY' || this.month.toUpperCase == 'JULE' || this.month.toUpperCase == 'AUGUST' || this.month.toUpperCase == 'OCTOBER' || this.month.toUpperCase == 'DECEMBER') this.daynum = 31
+    }
+}
+// time = new Date()
